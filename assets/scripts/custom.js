f991d5c5e630cb098f200a9ba189b271aa397894
Chris Fournier <christopher.fournier@panerabread.com>
2017-08-04 11:37:56 -0400

    Rebuilt Navigation
    Simplified the structure of the site navigation and removed excess
    javascript. However, navigational accessibility has also been impaired
    (as it was to begin with), but I hope to solve that in a future commit.

--- assets/scripts/custom.js
+++ assets/scripts/custom.js
@@ -7,6 +7,13 @@ $("#open-nav").animatedModal({
   color:'#ffffff'
 });
 
+$("#open-nav-wide").animatedModal({
+  modalTarget:'nav-modal-wide',
+  animatedIn:'slideInLeft',
+  animatedOut:'slideOutLeft',
+  color:'#ffffff'
+});
+
 //Pin Order Setup Bar
 $.fn.isOnScreen = function () {
     var win = $(window);
@@ -32,14 +39,14 @@ $(window).scroll(function () {
 });
 
 // Adaptive Color
-var stickyOffset = $("#btn-open-modal.open-nav-modal, #nav-color li.menu-item a, nav#a11y-menu ul.nav.level-1 li a, .user a.btn-primary").offset();
+var stickyOffset = $("#btn-open-modal.open-nav-modal, #nav-color li a, .user a.btn-primary").offset();
 var $contentDivs = $(".bg");
 $(document).scroll(function() {
   $contentDivs.each(function(k) {
     var _thisOffset = $(this).offset();
     var _actPosition = _thisOffset.top - $(window).scrollTop();
     if (_actPosition < stickyOffset.top && _actPosition + $(this).height() > 0) {
-      $("#btn-open-modal.open-nav-modal, #nav-color li.menu-item a, nav#a11y-menu ul.nav.level-1 li a, .user a.btn-sign-in, .user.is-authenticated .name a, .user.is-authenticated .rewards a, .user.is-authenticated .name a span.icon-dribbble, .user.is-authenticated .rewards a span.icon-award").removeClass("light dark").addClass($(this).hasClass("light") ? "light" : "dark");
+      $("#btn-open-modal.open-nav-modal, #nav-color li a, .user a.btn-sign-in, .user.is-authenticated .name a, .user.is-authenticated .rewards a, .user.is-authenticated .name a span.icon-dribbble, .user.is-authenticated .rewards a span.icon-award").removeClass("light dark").addClass($(this).hasClass("light") ? "light" : "dark");
       return false;
     }
   });
