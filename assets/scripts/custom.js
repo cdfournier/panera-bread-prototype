b56a17cc934cf3b7ef305d71966aab2af52fb8be
Chris Fournier <christopher.fournier@panerabread.com>
2017-08-10 12:44:58 -0400

    Added Profile page
    Made some css changes, but also disabled vertical navigation from home
    page.

--- assets/scripts/custom.js
+++ assets/scripts/custom.js
@@ -40,6 +40,23 @@ $(window).scroll(function () {
         $('.order-setup').removeClass('expand');
     }
 });
+
+
+//Hide vertical navigation
+/*
+$.fn.isOnScreen = function () {
+    var win = $(window);
+    var viewport = {
+        top: win.scrollTop(),
+        left: win.scrollLeft()
+    };
+    viewport.right = viewport.left + win.width();
+    viewport.bottom = viewport.top + win.height();
+    var bounds = this.offset();
+    bounds.right = bounds.left + this.outerWidth();
+    bounds.bottom = bounds.top + this.outerHeight();
+    return (!(viewport.right < bounds.left || viewport.left > bounds.right || viewport.bottom < bounds.top || viewport.top > bounds.bottom));
+};
 $(window).scroll(function () {
     if ($('#feature').isOnScreen() == true) {
         $('#cd-vertical-nav').addClass('hidden');
@@ -56,6 +73,7 @@ $(window).scroll(function () {
         $('#cd-vertical-nav').removeClass('hidden');
     }
 });
+*/
 
 
 // Adaptive Color
